54d319e5dfd04c8e25f87ad6ad74f2ea76d9ca83
Charles Vandendriessche <chvandendriessche@neomnia.net>
2025-06-03 15:42:47 +0200

    Delete install-kubesphere.sh
    delete file

--- install-kubesphere.sh
+++
@@ -1,5 +0,0 @@
-cd kubesphere-install
-chmod +x install-kubesphere.sh
-git add install-kubesphere.sh
-git commit -m "Ajout du script install-kubesphere.sh à la racine du dépôt"
-git push origin main
